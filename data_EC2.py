36558df60536ad953e66e9f06bd20d842d2be9be
Ivanysh Dmytro <dmytro_ivanysh@epam.com>
2015-09-20 18:26:56 +0300

    added more information to EC2 and Volumes data

--- data_EC2.py
+++ data_EC2.py
@@ -5,16 +5,16 @@ class data:
         self.Name = 'EC2'
         self.Priority = 3
         self.show = True
-        self.HeaderNames = ['Name', 'ELB name', 'IP address', 'State', 'Route53 name', 'Private IP', 'Public DNS', 'Instance type']
-        self.HeaderWidths = ['5', '2', '2', '1', '4', '2', '6', '2']
-        self.HeaderKeys = ['name', 'elb', 'ip_address', 'state', 'route53_name', 'private_ip_address', 'public_dns', 'type']
+        self.HeaderNames = ['ID', 'Name', 'ELB name', 'IP address', 'State', 'Route53 name', 'Private IP', 'Public DNS', 'Instance type', 'Security groups']
+        self.HeaderWidths = ['2', '5', '2', '2', '1', '4', '2', '6', '2', '4']
+        self.HeaderKeys = ['id', 'name', 'elb', 'ip_address', 'state', 'route53_name', 'private_ip_address', 'public_dns', 'type', 'sg']
         self.credentials = credentials
         self.Items = Items
         self.account = ''
         self.skipRegions = []
 
     def resultDict(self, instance, zones, elb_name=''):
-        res = {}
+        res = dict()
         if 'Name' in instance.tags:
             res['name'] = instance.tags['Name']
         else:
@@ -26,6 +26,7 @@ class data:
         res['type'] = instance.instance_type
         res['ssh_key'] = instance.key_name
         res['image_id'] = instance.image_id
+        res['sg'] = ','.join([group.name for group in instance.groups])
         res['placement'] = instance.placement
         res['private_ip_address'] = instance.private_ip_address
         res['elb'] = elb_name.split('.')[0].rsplit('-', 1)[0]
@@ -43,7 +44,7 @@ class data:
         return res
 
     def getAllItems(self, aws_key, aws_secret, Items):
-        result = {}
+        result = dict()
         regions = boto.ec2.regions(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret)
         for region in regions:
             if region.name in self.skipRegions:
@@ -66,7 +67,7 @@ class data:
         return result
 
     def getData(self):
-        EC2s = {}
+        EC2s = dict()
         for credential in self.credentials:
             self.account = credential[2]
             EC2s[credential[2]] = self.getAllItems(credential[0], credential[1], self.Items)
--- data_volumes.py
+++ data_volumes.py
@@ -7,26 +7,35 @@ class data:
         self.Name = 'Volumes'
         self.Priority = 9
         self.show = True
-        self.HeaderNames = ['ID', 'Attachment State', 'IOPS', 'Size', 'Create Time']
-        self.HeaderWidths = ['2', '2', '1', '1', '3']
-        self.HeaderKeys = ['name', 'attachment_state', 'iops', 'size', 'create_time']
+        self.HeaderNames = ['ID', 'Attachment State', 'IOPS', 'Size', 'Create Time', 'Instance Id', 'Instance name']
+        self.HeaderWidths = ['2', '2', '1', '1', '3', '2', '4']
+        self.HeaderKeys = ['name', 'attachment_state', 'iops', 'size', 'create_time', 'instance_id', 'instance_name']
         self.credentials = credentials
         self.Items = Items
         self.account = ''
         self.skipRegions = []
 
     def resultDict(self, volume):
-        res = {}
+        res = dict()
         res['name'] = volume.id
         res['attachment_state'] = volume.attachment_state()
         res['iops'] = str(volume.iops)
         res['size'] = str(volume.size)+'Gb'
         res['placement'] = volume.zone
         res['create_time'] = volume.create_time
+        if volume.attach_data:
+            res['instance_id'] = volume.attach_data.instance_id
+            for region in self.Items['EC2'][self.account].keys():
+                for instanceDict in self.Items['EC2'][self.account][region]:
+                    if res['instance_id'] == instanceDict['id']:
+                        res['instance_name'] = instanceDict['name']
+        else:
+            res['instance_id'] = 'none'
+            res['instance_name'] = 'none'
         return res
 
     def getAllItems(self, aws_key, aws_secret, Items):
-        result = {}
+        result = dict()
         regions = boto.ec2.regions(aws_access_key_id=aws_key, aws_secret_access_key=aws_secret)
         for region in regions:
             if region.name in self.skipRegions:
